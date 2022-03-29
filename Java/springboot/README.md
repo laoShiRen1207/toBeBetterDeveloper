0e8fb6b722f2d2652f4a1878aa5030734fb08f77
laoshiren <xiangdehua@pharmakeyring.com>
2022-03-29 21:54:38 +0800

    feat(springboot)

---
+++ Java/springboot/README.md
@@ -0,0 +1,11 @@
+# SpringBoot
+
+
+## run
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/eeec8447291c4c74a6dc49542b4355d3.png)
+
+[图片地址](https://img-blog.csdnimg.cn/eeec8447291c4c74a6dc49542b4355d3.png)
+
+
+## @EnableAutoConfiguration
---
+++ Java/springboot/src/main/java/com/laoshiren/hello/springboot/SpringBootMain.java
@@ -0,0 +1,25 @@
+package com.laoshiren.hello.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ProjectName:     toBeBetterDeveloper
+ * Package:         com.laoshiren.hello.springboot
+ * ClassName:       SpringBootMain
+ * Author:          laoshiren
+ * Git:             xiangdehua@pharmakeyring.com
+ * Description:     TBD
+ * Date:            2022/3/28 17:40
+ * Version:         1.0.0
+ */
+@SpringBootApplication
+public class SpringBootMain {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootMain.class, args);
+
+    }
+
+}
